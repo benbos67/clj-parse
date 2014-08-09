e011fefd6bc52bb1019dcc4771e8a0cd93ae42eb
Ben Bos <benbos67@gmail.com>
2014-08-09 14:03:07 +1000

    re-write parser using resource; easier to re-use

--- src/clj_parse/core.clj
+++ src/clj_parse/core.clj
@@ -4,8 +4,17 @@
 
 (defn get-from-html
   "returns a lazy sequence containing the content of tag(s) elem in file f"
-  [f elem]
-  (map html/text
-       (html/select
-        (html/html-resource (io/file f))
-        [elem])))
+  [res elem]
+  (let [res (html/html-resource (io/file f))]
+    (map html/text
+         (html/select
+          res
+          [elem]))))
+
+(defn parse-help-files
+  [dir]
+  (doseq dir
+    (
+;for each file f
+;get-from-html-1 (html/html-resource (io/file f)) :h6
+     ))
