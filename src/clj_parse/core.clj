cd9c7b260b8df498387f95ca7559b7ca30f81164
Ben Bos <benbos67@gmail.com>
2014-08-10 14:24:23 +1000

    Making headway, pulling this stuff apart into separate functions.

--- src/clj_parse/core.clj
+++ src/clj_parse/core.clj
@@ -5,16 +5,20 @@
 (defn get-from-html
   "returns a lazy sequence containing the content of tag(s) elem in file f"
   [res elem]
-  (let [res (html/html-resource (io/file f))]
-    (map html/text
-         (html/select
-          res
-          [elem]))))
+  (map html/text
+       (html/select
+        res
+        [elem])))
 
-(defn parse-help-files
-  [dir]
-  (doseq dir
-    (
-;for each file f
-;get-from-html-1 (html/html-resource (io/file f)) :h6
-     ))
+(defn get-files
+  ""
+  []
+  (filter #(not (.isDirectory %))
+          (file-seq (clojure.java.io/file "."))))
+
+(defn main
+  ""
+  []
+  (filter #(not-empty %)
+          (map #(get-from-html (html/html-resource %) :h1)
+               (get-files))))
